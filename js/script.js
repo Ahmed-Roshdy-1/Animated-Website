8f5992c10c46008bee045feac0ced2fdb269c392
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-08-11 19:13:04 +0200

    clean my code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+AOS.init({
+    delay: 200, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+   });
